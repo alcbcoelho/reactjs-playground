17ad4ebc9904eb57ce3cbcb83b1f9c42b33c4b3a
André Coêlho <alcbcoelho@gmail.com>
2023-06-20 16:03:36 -0300

    Registration form setup with React Hook Form

---
+++ notes/react-hook-form.md
@@ -0,0 +1,22 @@
+# React Hook Form
+
+```
+npm install react-hook-form
+```
+
+## React Hook Form DevTools
+
+```
+npm install -D @hookform/devtools
+```
+
+Allows us to see details whilst messing around with forms through a handy component (see RegistrationForm.jsx for details on how to implement it)
+
+![Alt text](devtool.png)
+
+# Validation rules
+React Hook form supports various HTML validation rules:
+
+- required
+- minLength & maxLength
+- min & max
--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,29 +1,78 @@
+import { DevTool } from "@hookform/devtools";   // devTool para React Hook Form
+import { useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom";
+
 export default function RegistrationForm() {
+    // hooks
+    const navigate = useNavigate();
+    const form = useForm();
+
+    const { register, control, handleSubmit, formState: { errors } } = form;    // the formState thing is the same as destructuring it separately afterwards:
+    // const { errors } = formState;
+    // The "errors" object contain errors for every field that has field validation.
+
+    const onSubmit = (data) => {
+        console.log(data);
+        navigate("/");
+    };
+
     return (
-        <form>
-            <div>
-                <label htmlFor="email">Email: </label>
-                <input type="email" id="email" />
-            </div>
-            <div>
-                <label htmlFor="username">Username: </label>
-                <input type="text" id="username" />
-            </div>
-            <div>
-                <label htmlFor="password">Password: </label>
-                <input type="password" id="password" />
-            </div>
-            <div>
-                <label htmlFor="terms-of-service">
-                    <input type="checkbox" id="terms-of-service" /> I agree with the Terms of Service
-                </label>
-            </div>
-            <div>
-                <label htmlFor="newsletter">
-                    <input type="checkbox" id="newsletter" /> I want to subscribe to our weekly newsletter
-                </label>
-            </div>
-            <button>Register</button>
-        </form>
+        <>  
+            <form onSubmit={handleSubmit(onSubmit)} noValidate /* noValidate: prevents browser validation, allowing React Hook Form to handle the validation of the fields */>
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" {...register("email", {
+                        required: "Email is required!",
+                        pattern: {
+                            value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                            message: "Invalid email format"
+                        }
+                    })} />
+                </div>
+                <p className="error">{errors.email?.message  /* se der ruim, solta mensagem */}</p>
+
+                <div>
+                    <label htmlFor="username">Username: </label>
+                    <input type="text" id="username" {...register("username", {
+                        required: "Username is required!"
+                        /* required: {
+                            value: true,
+                            message: "Username is required!"
+                        }   // could pass it like this as well */
+                    })} />
+                </div>
+                <p className="error">{errors.username?.message}</p>
+
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" {...register("password", {
+                        required: "Password is required!",
+                        minLength: {
+                            value: 5,
+                            message: "Password must have a minimum of 6 digits"
+                        }
+                    })} />
+                </div>
+                <p className="error">{errors.password?.message}</p>
+
+                <div>
+                    <label htmlFor="terms-of-service">
+                        <input type="checkbox" id="terms-of-service" {...register("terms-of-service", {
+                            required: "You must agree with our Terms of Service"
+                        })} /> I agree with the Terms of Service
+                    </label>
+                </div>
+                <p className="error">{errors["terms-of-service"]?.message}</p>
+
+                <div>
+                    <label htmlFor="newsletter">
+                        <input type="checkbox" id="newsletter" {...register("newsletter")} /> Subscribe to our weekly newsletter
+                    </label>
+                </div>
+
+                <button>Register</button>
+            </form>
+            <DevTool control={control} />
+        </>
     )
 }
