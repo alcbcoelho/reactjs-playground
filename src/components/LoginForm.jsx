1efa3259c5102d60bb69b60f65d5669255fa597c
André Coêlho <alcbcoelho@gmail.com>
2023-06-21 16:40:04 -0300

    Basic login form setup with React Hook Form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,15 +1,45 @@
+import { DevTool } from "@hookform/devtools";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginForm() {
+    const navigate = useNavigate();
+    const form = useForm();
+    const { register, control, handleSubmit, formState: { errors } } = form;
+
+    const onSubmit = (data) => {
+        console.log(data);
+        navigate("/");
+    }
+
     return (
-        <form>
-            <div>
-                <label htmlFor="username">Username: </label>
-                <input type="text" id="username" />
-            </div>
-            <div>
-                <label htmlFor="password">Password: </label>
-                <input type="password" id="password" />
-            </div>
-            <button>Login</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <div>
+                    <label htmlFor="username">Username: </label>
+                    <input type="text" id="username" {...register("username", {
+                        required: {
+                            value: true,
+                            message: "Username is required!"
+                        }
+                    })}/>
+                </div>
+                <p className="error">{errors.username?.message}</p>
+
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" {...register("password", {
+                        required: {
+                            value: true,
+                            message: "Password is required!"
+                        }
+                    })} />
+                </div>
+                <p className="error">{errors.password?.message}</p>
+
+                <button>Login</button>
+            </form>
+            {/* <DevTool control={control} /> */}
+        </>
     )
 }
